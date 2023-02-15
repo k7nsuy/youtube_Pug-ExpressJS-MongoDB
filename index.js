751a26ffaf422b28bf84d8be5a6eacae4f8c6e94
Sangwoo Yu <76999175+k7nsuy@users.noreply.github.com>
2023-02-16 00:59:50 +0900

    new

---
+++ index.js
@@ -0,0 +1,19 @@
+import express from 'express'
+
+const app = express()
+const PORT = 3333
+
+
+
+const handHome = (req,res,nex) => {
+    console.log(req);
+    res.end()
+}
+
+app.get('/', handHome)
+
+
+const handleListening = () => {
+    console.log(`This server is running on port 3333✅`);
+}
+app.listen(PORT, handleListening)
